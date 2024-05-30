fa8314f5d7866adc358c1d440ac45191c1593c63
Matheus Batista <mbatista.sarti@gmail.com>
2024-05-30 20:38:21 -0300

    :sparkles: created email model to be used in service

---
+++ EmailService/Notification/Models/Email.cs
@@ -0,0 +1,8 @@
+namespace Notification.Models;
+
+public class Email
+{
+    public String? Subject { get; set; } 
+    public String? Content { get; set; } 
+    public String? ToEmail { get; set; } 
+}
